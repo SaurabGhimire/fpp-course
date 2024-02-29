e5bc81ca745a771ed3ddd696a7ab5b9955b73a2a
Saurab Ghimire <sourrab@gmail.com>
2024-02-28 20:40:46 -0600

    fpp assignments first commit

---
+++ src/assignment2/Prog1.java
@@ -0,0 +1,17 @@
+package assignment2;
+
+public class Prog1 {
+    public static void main(String[] args) {
+        int randomInt = RandomNumbers.getRandomInt(1, 9);
+        double res = computePower(Math.PI, randomInt);
+        System.out.println("PI ^ " + randomInt + ": "+ res);
+
+        randomInt = RandomNumbers.getRandomInt(3, 14);
+        res = computePower(randomInt, Math.PI);
+        System.out.println(randomInt  + "^ PI: " + res);
+    }
+
+    static double computePower(double base, double power){
+        return  Math.pow(base, power);
+    }
+}
