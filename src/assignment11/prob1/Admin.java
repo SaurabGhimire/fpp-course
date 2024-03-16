c39ea01a3e470de1bb508ab7bd267bbbc4ecad01
Saurab Ghimire <sourrab@gmail.com>
2024-03-16 15:27:48 -0500

    assign11 prob1 src code setup

---
+++ src/assignment11/prob1/Admin.java
@@ -0,0 +1,15 @@
+package assignment11.prob1;
+
+import java.util.*;
+
+public class Admin {
+    public static HashMap<Key, Student> processStudents(List<Student> students) {
+        //implement
+        return null;
+    }
+
+    public static double computeAverageGPA(HashMap<Key, Student> maps) {
+        //implements
+        return 0.0;
+    }
+}
